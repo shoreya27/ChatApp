a02bc434aeb172985906f457205298d09a04ca7b
shoreya <gupta.shoreya2707@gmail.com>
2021-07-09 23:32:44 +0530

    Added pod CLTypingLabel for Animation purpose

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,27 +7,29 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = ""
-       let text = "⚡️FlashChat"
-        var charIndex = 0.0
-        for letter in text{
-            print(charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-                print(self.titleLabel.text!)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
+        
+        // titleLabel.text = ""
+       //let text = "⚡️FlashChat"
+       //var charIndex = 0.0
+        //for letter in text{
+          //  print(charIndex)
+            //print(letter)
+            //Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+              //  self.titleLabel.text?.append(letter)
+               // print(self.titleLabel.text!)
+            //}
+            //charIndex += 1
+        //}
     }
     
-
 }
